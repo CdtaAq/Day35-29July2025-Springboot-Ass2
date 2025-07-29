01d2ca07538e87302ea3c90c74e81818506735ac
chris Taylor <ctay456a@gmail.com>
2025-07-29 17:24:30 -0400

    Create EmployeeRepository.java

---
+++ src/main/java/com/synergisticit/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.synergisticit.repository;
+
+import com.synergisticit.domain.Employee;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+}
